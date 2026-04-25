62e5cd337e8308e6a27ba93befebe577bef162ab
jhutson256 <hut24007@byui.edu>
2026-04-25 11:01:55 -0600

    prep 1 and prep 2 assignments completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string jph_firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string jhp_lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {jhp_lastName}, {jph_firstName} {jhp_lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string jph_value = Console.ReadLine();
+        int jph_percent = int.Parse(jph_value);
+
+        string jph_letterGrade = "";
+
+        if (jph_percent >= 90)
+        {
+            jph_letterGrade = "A";
+        }
+        else if (jph_percent >= 80)
+        {
+            jph_letterGrade = "B";
+        }
+        else if (jph_percent >= 70)
+        {
+            jph_letterGrade = "C";
+        }
+        else if (jph_percent >= 60)
+        {
+            jph_letterGrade = "D";
+        }
+        else
+        {
+            jph_letterGrade = "F";
+        }
+
+        string jph_letterSign = "";
+        int jph_remainder = jph_percent % 10;
+
+        if (jph_letterGrade != "A" && jph_letterGrade != "F")
+        {
+            if (jph_remainder >= 7)
+            {
+                jph_letterSign = "+";
+            }
+            else if (jph_remainder <= 3)
+            {
+                jph_letterSign = "-";
+            }
+        }
+        else if (jph_letterGrade == "A")
+        {
+            if (jph_remainder <= 3)
+            {
+                jph_letterSign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {jph_letterGrade}{jph_letterSign}");
+
+        if (jph_percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
